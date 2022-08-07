4d35b1886d7f24f4145eafd1b1194d81c43f0aef
KTeam-Devs <kirito234875@gmail.com>
2022-08-07 12:39:41 +0530

    renderer

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -5,6 +5,10 @@
 #include "core/kmemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
+
+#include "renderer/renderer_frontend.h"
+
 typedef struct application_state {
     game* game_inst;
     b8 is_running;
@@ -12,15 +16,15 @@ typedef struct application_state {
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
+
 static b8 initialized = FALSE;
 static application_state app_state;
-
 // Event handlers
 b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
 b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
-
 b8 application_create(game* game_inst) {
     if (initialized) {
         KERROR("application_create called more than once.");
@@ -43,11 +47,9 @@ b8 application_create(game* game_inst) {
         KERROR("Event system failed initialization. Application cannot continue.");
         return FALSE;
     }
-
     event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
     event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
     event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
-
     if (!platform_startup(
             &app_state.platform,
             game_inst->app_config.name,
@@ -57,6 +59,13 @@ b8 application_create(game* game_inst) {
             game_inst->app_config.start_height)) {
         return FALSE;
     }
+
+    // Renderer startup
+    if (!renderer_initialize(game_inst->app_config.name, &app_state.platform)) {
+        KFATAL("Failed to initialize renderer. Aborting application.");
+        return FALSE;
+    }
+
     // Initialize the game.
     if (!app_state.game_inst->initialize(app_state.game_inst)) {
         KFATAL("Game failed to initialize.");
@@ -66,82 +75,126 @@ b8 application_create(game* game_inst) {
     initialized = TRUE;
     return TRUE;
 }
+
 b8 application_run() {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 running_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     KINFO(get_memory_usage_str());
+
     while (app_state.is_running) {
         if (!platform_pump_messages(&app_state.platform)) {
             app_state.is_running = FALSE;
         }
+
         if (!app_state.is_suspended) {
             if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
-                KFATAL("Game update failed, shutting down.");
-                app_state.is_running = FALSE;
-                break;
+                // Update clock and get delta time.
+                clock_update(&app_state.clock);
+                f64 current_time = app_state.clock.elapsed;
+                f64 delta = (current_time - app_state.last_time);
+                f64 frame_start_time = platform_get_absolute_time();
+
+                if (!app_state.game_inst->update(app_state.game_inst, (f32)delta)) {
+                    KFATAL("Game update failed, shutting down.");
+                    app_state.is_running = FALSE;
+                    break;
+                }
+
+                // Call the game's render routine.
+                if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+                    if (!app_state.game_inst->render(app_state.game_inst, (f32)delta)) {
+                        KFATAL("Game render failed, shutting down.");
+                        app_state.is_running = FALSE;
+                        break;
+                    }
+
+                    // TODO: refactor packet creation
+                    render_packet packet;
+                    packet.delta_time = delta;
+                    renderer_draw_frame(&packet);
+
+                    // Figure out how long the frame took and, if below
+                    f64 frame_end_time = platform_get_absolute_time();
+                    f64 frame_elapsed_time = frame_end_time - frame_start_time;
+                    running_time += frame_elapsed_time;
+                    f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+                    if (remaining_seconds > 0) {
+                        u64 remaining_ms = (remaining_seconds * 1000);
+
+                        // If there is time left, give it back to the OS.
+                        b8 limit_frames = FALSE;
+                        if (remaining_ms > 0 && limit_frames) {
+                            platform_sleep(remaining_ms - 1);
+                        }
+
+                        frame_count++;
+                    }
+
+                    // NOTE: Input update/state copying should always be handled
+                    // after any input should be recorded; I.E. before this line.
+                    // As a safety, input is the last thing to be updated before
+                    // this frame ends.
+                    input_update(0);
+                    input_update(delta);
+
+                    // Update last time
+                    app_state.last_time = current_time;
+                }
             }
-            // Call the game's render routine.
-            if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
-                KFATAL("Game render failed, shutting down.");
-                app_state.is_running = FALSE;
-                break;
-            }
-            // NOTE: Input update/state copying should always be handled
-            // after any input should be recorded; I.E. before this line.
-            // As a safety, input is the last thing to be updated before
-            // this frame ends.
-            input_update(0);
-        }
-    }
 
-    app_state.is_running = FALSE;
-
-    // Shutdown event system.
-    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
-    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
-    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
-    event_shutdown();
-    input_shutdown();
-
-    platform_shutdown(&app_state.platform);
-
-    return TRUE;
-}
-
-b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
-    switch (code) {
-        case EVENT_CODE_APPLICATION_QUIT: {
-            KINFO("EVENT_CODE_APPLICATION_QUIT recieved, shutting down.\n");
             app_state.is_running = FALSE;
+            // Shutdown event system.
+            event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+            event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+            event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+            event_shutdown();
+            input_shutdown();
+
+            renderer_shutdown();
+
+            platform_shutdown(&app_state.platform);
+
             return TRUE;
         }
-    }
-
-    return FALSE;
-}
-
-b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
-    if (code == EVENT_CODE_KEY_PRESSED) {
-        u16 key_code = context.data.u16[0];
-        if (key_code == KEY_ESCAPE) {
-            // NOTE: Technically firing an event to itself, but there may be other listeners.
-            event_context data = {};
-            event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
-
-            // Block anything else from processing this.
-            return TRUE;
-        } else if (key_code == KEY_A) {
-            // Example on checking for a key
-            KDEBUG("Explicit - A key pressed!");
-        } else {
-            KDEBUG("'%c' key pressed in window.", key_code);
+        b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
+            switch (code) {
+                case EVENT_CODE_APPLICATION_QUIT: {
+                    KINFO("EVENT_CODE_APPLICATION_QUIT recieved, shutting down.\n");
+                    app_state.is_running = FALSE;
+                    return TRUE;
+                }
+            }
+            return FALSE;
         }
-    } else if (code == EVENT_CODE_KEY_RELEASED) {
-        u16 key_code = context.data.u16[0];
-        if (key_code == KEY_B) {
-            // Example on checking for a key
-            KDEBUG("Explicit - B key released!");
-        } else {
-            KDEBUG("'%c' key released in window.", key_code);
+        b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+            if (code == EVENT_CODE_KEY_PRESSED) {
+                u16 key_code = context.data.u16[0];
+                if (key_code == KEY_ESCAPE) {
+                    // NOTE: Technically firing an event to itself, but there may be other listeners.
+                    event_context data = {};
+                    event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+                    // Block anything else from processing this.
+                    return TRUE;
+                } else if (key_code == KEY_A) {
+                    // Example on checking for a key
+                    KDEBUG("Explicit - A key pressed!");
+                } else {
+                    KDEBUG("'%c' key pressed in window.", key_code);
+                }
+            } else if (code == EVENT_CODE_KEY_RELEASED) {
+                u16 key_code = context.data.u16[0];
+                if (key_code == KEY_B) {
+                    // Example on checking for a key
+                    KDEBUG("Explicit - B key released!");
+                } else {
+                    KDEBUG("'%c' key released in window.", key_code);
+                }
+            }
+            return FALSE;
         }
-    }
-    return FALSE;
-}
